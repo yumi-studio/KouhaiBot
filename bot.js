04f21e00d813620131ceed5e66368eaeba6eb425
Yumi <caubedondoc.ls01@gmail.com>
2018-02-17 22:52:48 +0700

    .

--- bot.js
+++ bot.js
@@ -8,12 +8,20 @@ client.on('ready', () => {
 	console.log('I am ready!');
 });
 
+client.on('guildMemberAdd', member =>{
+	const channel = member.guild.find('name','member-log');
+	if(!channel) return;
+	channel.send('Welcome to '+member.guild.name+',${member} senpai');
+});
+
 client.on('message', message => {
 	var sender= message.author;
 	var em = new Discord.RichEmbed();
 	if(message.content.substring(0,1) === prefix){
 		var cmd = message.content.substring(1,message.content.length).split(' ');
 		switch(cmd[0]){
+			
+			/*meme*/
 			case 'hhh':
 				em.setImage('https://i.imgur.com/ojjWsjK.jpg');
 				message.channel.send(em);
@@ -22,6 +30,12 @@ client.on('message', message => {
 				em.setImage('https://i.imgur.com/nacjQtW.jpg');
 				message.channel.send(em);
 				return;
+			case 'yaoming':
+				em.setImage('https://i.imgur.com/5218yLa.jpg');
+				message.channel.send(em);
+				return;
+			
+			/*--------------------------------*/
 			case 'roll':
 				var opt = cmd[1].split('-');
 				var num= opt.length;
@@ -29,11 +43,11 @@ client.on('message', message => {
 					message.channel.send(opt[Math.floor(Math.random()*num)]);
 				}
 				return;
-			/* case 'quote':
-				var file = require('./quote.json');
+			case 'quote':
+				var objq = JSON.parse(fs.readFileSync("./quote.json","utf8"));
 				var count=0;
 				var q ='';
-				if(cmd[1]==='add'){
+				/* if(cmd[1]==='add'){
 					for(var i=3;i<cmd.length;i++){
 						q = q +' '+ cmd[i];
 					}
@@ -42,14 +56,11 @@ client.on('message', message => {
 					fs.writeFile('./quote.json',JSON.stringify(file),(err)=>console.error);
 					message.channel.send(file.name+':'+file.text);
 					return;
+				} */
+				if(cmd[1]===objq.name){
+					message.channel.send(objq.text);
 				}
-				while(true){
-					if(file.name===cmd[1]){
-						message.channel.send(file.text);
-						break;
-					}
-				}
-				return; */
+				return;
 			case 'abcdef':
 				if(sender.id!==bossId){
 					message.channel.send('you dont have enough permission.');
@@ -59,11 +70,13 @@ client.on('message', message => {
 				return;
 			case 'watashi?':
 				em.setColor(Math.floor(Math.random()*16777216));
+				em.setTitle('**'+message.member.displayName+'**');
 				em.addField('**Realname** ',sender.username);
+				em.addBlankField();
 				em.addField('**Joined Time**',sender.createdAt);
 				em.setThumbnail(sender.avatarURL);
 				message.channel.send(em);
-				return;
+				return; 
 			default:
 				message.channel.send('Command not found');
 		}	
