0439fe7762aa39ba8b12016e3b596ac31bba0ea5
Yumi <caubedondoc.ls01@gmail.com>
2018-02-20 22:08:11 +0700

    .

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 
-const redis = require('ioredis');
+const Redis = require('ioredis');
 const JSONStore = require('./');
 
 const redis = new Redis();
