f21e8982117a47c6b809cde2418d5d42400246f8
Yumi <caubedondoc.ls01@gmail.com>
2018-03-01 10:34:16 +0700

    update werewolve game

--- bot.js
+++ bot.js
@@ -5,7 +5,10 @@ const client = new Discord.Client();
 const fs = require('fs');
 
 var rdc = require('redis').createClient(process.env.REDIS_URL);
-
+var Game = {
+		stat: 0,
+		Player: []
+	}
 const prefix = '!';
 const bossId = process.env.BOSS_ID;
 
@@ -23,23 +26,26 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	var sender= message.author;
+	var guild = message.guild;
+	var channel = message.channel;
+	var content = message.content;
 	var em = new Discord.RichEmbed();
-	if(message.content.substring(0,1) === prefix && message.channel.type!=='dm'){
-		var cmd = message.content.substring(1,message.content.length).split(' ');
+	if(content.substring(0,1) === prefix && channel.type!=='dm'){
+		var cmd = content.substring(1,message.content.length).split(' ');
 		switch(cmd[0]){
 			
 			/*meme*/
 			case 'hhh':
 				em.setImage('https://i.imgur.com/ojjWsjK.jpg');
-				message.channel.send(em);
+				channel.send(em);
 				return;
 			case 'pff':
 				em.setImage('https://i.imgur.com/nacjQtW.jpg');
-				message.channel.send(em);
+				channel.send(em);
 				return;
 			case 'ym':
 				em.setImage('https://i.imgur.com/5218yLa.jpg');
-				message.channel.send(em);
+				channel.send(em);
 				return;
 			
 			/*--------------------------------*/
@@ -48,17 +54,16 @@ client.on('message', message => {
 				var opt = cmd[1].split('-');
 				var num= opt.length;
 				if(num>1){
-					message.channel.send(opt[Math.floor(Math.random()*num)]);
+					channel.send(opt[Math.floor(Math.random()*num)]);
 				}
 				return;
 			/*Quote*/
 			case 'quote':
-				var guildId = message.guild.id;
 				var objq =[];
 				if(cmd[1]==='add'){
 					/* Check permission */
 					if(sender.id===bossId || message.member.permissions.FLAGS==='ADMINISTRATOR'){
-						rdc.get("quote"+guildId,function(err,reply){
+						rdc.get("quote"+guild.id,function(err,reply){
 							if(reply!==null){
 								objq = JSON.parse(reply.toString());
 							}
@@ -69,24 +74,24 @@ client.on('message', message => {
 						for(var i=3;i<cmd.length;i++){
 							q = q +' '+ cmd[i];
 						}
-						var newobj = {
+						let newobj = {
 							name: cmd[2],
 							text: q
 						};
 						
 						objq.push(newobj);
 						objq = JSON.stringify(objq);
-						rdc.set('quote'+guildId,objq,function(){
-							message.channel.send("New quote **"+newobj.name+"** is added.");
+						rdc.set('quote'+guild.id,objq,function(){
+							channel.send("New quote **"+newobj.name+"** is added.");
 						});
 					}else{
-						message.channel.send("You dont have permission to use this command.");
+						channel.send("You dont have permission to use this command.");
 					}
 					
 					return;
 				}
 				
-				rdc.get('quote'+guildId,function(err,reply){
+				rdc.get('quote'+guild.id,function(err,reply){
 					if(reply!==null){
 						objq = JSON.parse(reply.toString());
 						var found = objq.find(function(element){
@@ -96,17 +101,17 @@ client.on('message', message => {
 							em.setTitle("**"+found.name+"**");
 							em.setDescription('_"'+found.text+'"_');
 							em.setColor(Math.floor(Math.random()*16777216));
-							message.channel.send(em);
+							channel.send(em);
 						}
 					}else{
-						message.channel.send("This server dont have any quote");
+						channel.send("This server dont have any quote");
 					}
 				});
 				return;
 			/*Return id of an user*/
 			case 'abcdef':
 				if(sender.id!==bossId){
-					message.channel.send('you dont have enough permission.');
+					channel.send('you dont have enough permission.');
 					return;
 				}
 				sender.send(message.mentions.users.first().username+':'+message.mentions.users.first().id);
@@ -120,41 +125,65 @@ client.on('message', message => {
 				em.addBlankField();
 				em.addField('**Joined Time**',sender.createdAt);
 				em.setThumbnail(sender.avatarURL);
-				message.channel.send(em);
+				channel.send(em);
 				return; 
 			
 			/*Invite the bot*/
 			case 'inviteme':
-				message.channel.send('https://discordapp.com/api/oauth2/authorize?client_id=413384938965172255&permissions=0&scope=bot');
+				channel.send('https://discordapp.com/api/oauth2/authorize?client_id=413384938965172255&permissions=0&scope=bot');
 				return;
 				
 			case "name":
 				if(isboss(sender.id)){
 					client.user.setUsername(cmd[1]);
 				}
+				return;
 				
 			/*Werewolve*/
-			case 'masoi':{
-				if(sender.id!==bossId){
-					message.channel.send('you dont have enough permission.');
+			case "masoi":{
+				if(!isboss(sender.id)){
+					channel.send('you dont have enough permission.');
 					return;
+				}else{
+					if()
+					if(channel.name!=="Game"){
+						channel.send("Please using #Game_channel");
+						return;
+					}
 				}
-				var cha= [];
-				var role = 
-				message.channel.send("Bắt đầu game ma sói, gõ **masoi join** để tham gia");
-				while(true){
-					client.on('message', gamemes =>{
-						if(gamemes.content === cmd[0]+' join'){
-							cha.push({
-								name: gamemes.author.id
-							});
-						}
-					});
-				}
+				
+				rdc.set("masoi"+guild.id,JSON.stringify(Game),function(){
+					channel.send("Game start! Type **!join** to join");
+				});
+				return;
 			}
-			default:
-				message.channel.send('Command not found');
-		}	
+			case "join":
+				rdc.get("masoi"+guild.id,function(err,reply){
+					if(reply!==undefined){
+						Game = JSON.parse(reply.toString());
+						if(Game.stat===1){
+							Game.Player.push({
+								id: sender.id,
+								role: "none"
+							});
+							rdc.set("masoi"+guild.id,JSON.stringify(Game),function(){
+								channel.send(message.member.displayName+" joined");
+							})
+						}
+					}
+				}
+				return;
+			case "players":
+				rdc.get("masoi"+guild.id,function(err,reply){
+					if(reply!==undefined){
+						Game = JSON.parse(reply.toString());
+						for(var a =0;a<Game.Player.length;a++){
+							em.addField(""+(a+1)+"."+guild.members.find("id",Game.Player[a].id).displayName,"");
+						}
+					}
+				}
+				return;
+			}	
 	}
 });
 
