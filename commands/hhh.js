fb6e863f4d1c0476eae956353e2329e16ed094de
Yumi <caubedondoc.ls01@gmail.com>
2018-03-12 18:56:29 +0700

    .

--- commands/hhh.js
+++ commands/hhh.js
@@ -1,5 +1,5 @@
 exports.run = (client,message,cmd) =>{
-	var Discord = require("../discord.js");
+	var Discord = require("../discord.js/");
 	var em = new Discord.RichEmbed();
 	em.setImage('https://i.imgur.com/ojjWsjK.jpg');
 	message.channel.send(em);
