3f85c78a981e2511a4975c804668369d68a7fc40
Yumi <caubedondoc.ls01@gmail.com>
2018-03-13 12:27:11 +0700

    .

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,6 @@
 exports.run = (Discord,rdc,client,message,cmd) =>{
 	let list = cmd.split("-");
-	let num = list.lenght;
+	let num = list.length;
 	if(num>0){
 		message.channel.send(list[Math.floor(Math.random()*num)]);
 	}
