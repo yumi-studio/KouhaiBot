8c25780ea2c00553caa7d6ec397b9865cc3364b4
Yumi <caubedondoc.ls01@gmail.com>
2018-03-15 15:44:33 +0700

    .

--- commands/command.js
+++ commands/command.js
@@ -1,49 +1,12 @@
 exports.run = (Discord,rdc,client,message,cmd) =>{
 	let list =[];
-	let option = cmd.split(" ")[0];
+	let arg1 = cmd.split(" ")[0];
+	let arg2 = cmd.substring(arg1.length+1);
 	let sender = message.author;
 	let perm =  message.member.permissions.FLAGS;
 	let guild = message.guild;
 	let channel = message.channel;
-	switch(option){
-		case "add":
-			if(sender.id!==process.env.BOSS_ID && perm!=="ADMINISTRATOR") return;
-			rdc.get("cmd"+guild.id,function(err,reply){
-				if(reply!==null){
-					list = JSON.parse(reply.toString());
-				}
-				if(cmd.indexOf("|")===-1) return;
-				let newc = {
-					name: cmd.substring(option.length+1).split("|")[0], 
-					content: cmd.substring(cmd.split("|")[0].length+1)
-				}
-				let found = list.find(m=>m.name===newc.name);
-				if(found!==undefined){
-					channel.send("**"+newc.name+"** is in database. Please delete it before add new one.");
-					return;
-				}
-				list.push(newc);
-				rdc.set("cmd"+guild.id,JSON.stringify(list),()=>{
-					channel.send("**"+newc.name+"** is added.");
-				});
-			});
-			return;
-		case "del":
-			if(sender.id!==process.env.BOSS_ID && perm!=="ADMINISTRATOR") return;
-			rdc.get("cmd"+guild.id,function(err,reply){
-				if(reply!==null){
-					list = JSON.parse(reply.toString());
-				}else{
-					return;
-				}
-				let found = list.findIndex(m=>m.name===cmd.substring(option.length+1));
-				if(found===-1) return;
-				list.splice(found,1);
-				rdc.set("cmd"+guild.id,JSON.stringify(list),()=>{
-					channel.send("**"+cmd.substring(option.length+1)+"** is deleted.");
-				});
-			});
-			return;
+	switch(arg1){
 		case "list":
 			rdc.get("cmd"+guild.id,function(err,reply){
 				if(reply===null)	return;
@@ -52,7 +15,48 @@ exports.run = (Discord,rdc,client,message,cmd) =>{
 				list.forEach(m => {listname=listname + ", "+m.name});
 				listname=listname.substring(2);
 				channel.send("`"+listname+"`");
-			});
+			}); 
 			return;
+		default:
+			if(arg2 === null){
+				del();
+			}else{
+				set();
+			}
+	}
+
+	function set(){
+		if(sender.id!==process.env.BOSS_ID && perm!=="ADMINISTRATOR") return;
+		rdc.get("cmd"+guild.id,function(err,reply){
+			if(reply===null) return;
+			list = JSON.parse(reply.toString());
+			let newc = {
+				name: arg1, 
+				content: arg2
+			}
+			let found = list.findIndex(m=>m.name===newc.name);
+			if(found===-1){
+				list.push(newc);
+				channel.send("**"+arg1+"** is added.");
+			}else{
+				list.splice(found,1,newc);
+				channel.send("**"+arg1+"** is modified.");
+			}
+			rdc.set("cmd"+guild.id,JSON.stringify(list),()=>{});
+		});
+	}
+
+	function del(){
+		if(sender.id!==process.env.BOSS_ID && perm!=="ADMINISTRATOR") return;
+		rdc.get("cmd"+guild.id,function(err,reply){
+			if(reply===null) return;
+			list = JSON.parse(reply.toString());
+			let found = list.findIndex(m=>m.name===arg1);
+			if(found===-1) return;
+			list.splice(found,1);
+			rdc.set("cmd"+guild.id,JSON.stringify(list),()=>{
+				channel.send("**"+arg1+"** is deleted.");
+			});
+		});
 	}
 }
