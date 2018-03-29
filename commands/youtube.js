203af7b38f8236005c4b5cc2289f7dc6e0eea44d
Yumi <caubedondoc.ls01@gmail.com>
2018-03-29 19:00:38 +0700

    .

--- commands/youtube.js
+++ commands/youtube.js
@@ -3,9 +3,9 @@ exports.run = (Discord,rdc,client,message,args) =>{
     var src = `https://www.googleapis.com/youtube/v3/search?part=id&maxResults=25&q=${encodeURI(args)}&type=video&key=${process.env.APIKEY}`
     https.get(src,function(data){
         // var rd = Math.floor(Math.random()*data.items.length);
-        var em = new Discord.RichEmbed();
-        em.setURL("https://youtube.conm/watch?v="+data.items[0].id.videoId);
+        // var em = new Discord.RichEmbed();
+        // em.setURL("https://youtube.conm/watch?v="+data.items[0].id.videoId);
         console.log(data);
-        message.channel.send(em);
+        // message.channel.send(em);
     });
 }
