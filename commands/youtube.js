74bded7aec6c2693067ff49bc7bfec145c681c7d
Yumi <caubedondoc.ls01@gmail.com>
2018-03-29 18:34:17 +0700

    ,

--- commands/youtube.js
+++ commands/youtube.js
@@ -2,7 +2,7 @@ exports.run = (Discord,rdc,client,message,args) =>{
     const https = require("https");
     var src = `https://www.googleapis.com/youtube/v3/search?part=id&maxResults=25&q=${encodeURI(args)}&type=video&key=${process.env.APIKEY}`
     https.get(src,function(data){
-        var rd = Math.floor(Math.random()*data.items.lenght);
+        var rd = Math.floor(Math.random()*data.items.length);
         var em = new Discord.RichEmbed();
         em.setURL("https://youtube.conm/watch?v="+data.items[rd].id.videoId);
         message.channel.send(em);
