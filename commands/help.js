9b36128bc67ba9f4f65ccd0d1b3c30030e59cd5e
Yumi <caubedondoc.ls01@gmail.com>
2018-04-26 21:35:52 +0700

    ..

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,6 @@
 exports.run = (Discord,rdc,client,message,cmd) =>{
 	let em = new Discord.RichEmbed();
 	em.setTitle("KOUHAIBOT COMMANDS LIST");
-	em.setUrl("https://yumichannel.github.io/discord");
+	em.setURL("https://yumichannel.github.io/discord");
 	message.channel.send(em);
-	message.channel.send("abcdef");
 }
