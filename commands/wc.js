2515a2d9b2082d185860fb1253557ba3bc6adb72
Yumi <caubedondoc.ls01@gmail.com>
2018-04-26 22:57:14 +0700

    ,.,.

--- commands/wc.js
+++ commands/wc.js
@@ -12,21 +12,31 @@ exports.run = (Discord,rdc,client,message,cmd) =>{
             list = JSON.parse(reply.toString());
         }
         switch(opt){
-            case "a":
+            case "a": //add new
                 if(wcmsg.lenght<=0) return;
                 list.push(wcmsg);
                 rdc.set("welcome"+guild.id,JSON.stringify(list),()=>{
                     channel.send("Added new greeting message! Check greeting list with **!wc l**");
                 });
                 return;
-            case "x":
+            case "x": //delete all
                 list = [];
                 rdc.set("welcome"+guild.id,JSON.stringify(list),()=>{});
                 return;
-            case "l":
+            case "d": //delete single
+                try {
+                    let found = list.findIndex(m=>m===wcmsg);
+                    if(found===-1) return;
+                    list.splice(found,1);
+                    rdc.set("cmd"+guild.id,JSON.stringify(list),()=>{});
+                } catch (error) {
+                    channel.send("Syntax ERROR.");
+                }
+                return;
+            case "l": //list
                 let list2 ="";
                 for(let i=0;i<list.length;i++){
-                    list2 = list2 + `${i}:${list[i]} /n`; 
+                    list2 = list2 + `${i}:${list[i]}\n`; 
                 }
                 channel.send("```"+list2+"```");
                 return;
