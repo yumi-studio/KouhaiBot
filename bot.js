ec59cc494281f137c1fd1f099db5bd2d01ae6c99
Yumi <caubedondoc.ls01@gmail.com>
2018-05-09 21:11:46 +0700

    ....

--- bot.js
+++ bot.js
@@ -88,53 +88,29 @@ client.on('message', message=>{
 client.on('message', message=>{
 	if(!message.content.startsWith(prefix)) return;
 	if (!message.guild) return;
-
-  if (message.content === prefix+'join') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => {
-		  console.log('I have successfully connected to the channel!');
-		  const dispatcher = connection.playStream(
-			  yt("https://www.youtube.com/watch?v=Y_c-gwnuiz8",{
-				  filter: 'audioonly',
-				  resolution: '360p'
-			  }),
-			  {
-				  bitrate: 320
-			  }
-		  );
-        })
-        .catch(console.log);
-    } else {
-      console.log('You need to join a voice channel first!');
-    }
-  }
-});
-
-client.on('message', message=>{
-	if(!message.content.startsWith(prefix)) return;
-	if (!message.guild) return;
-
-  if (message.content === prefix+'join') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => {
-		  console.log('I have successfully connected to the channel!');
-		  const dispatcher = connection.playStream(
-			  yt("https://www.youtube.com/watch?v=Y_c-gwnuiz8",{
-				  filter: 'audioonly',
-				  resolution: '360p'
-			  }),
-			  {
-				  bitrate: 320
-			  }
-		  );
-        })
-        .catch(console.log);
-    } else {
-      console.log('You need to join a voice channel first!');
-    }
-  }
+  	if (message.content.substring(0,5) === prefix+'play') {
+		var yturl = message.content.substring(6);
+		if (message.member.voiceChannel) {
+		message.member.voiceChannel.join()
+			.then(connection => {
+				console.log('I have successfully connected to the channel!');
+				const dispatcher = connection.playStream(
+					yt(yturl,{
+						filter: 'audioonly',
+						resolution: '360p',
+						quality: 'highestaudio'
+					}),
+					{
+						bitrate: 'auto'
+					}
+				);
+				dispatcher.on('end',()=>{message.member.voiceChannel.leave()});
+			})
+			.catch(console.log);
+		} else {
+		console.log('You need to join a voice channel first!');
+		}
+  	}
 });
 
 });
