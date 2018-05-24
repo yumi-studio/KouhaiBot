2d837717a8d3c89e8ae5000240ed56be7f259b92
Yumi <caubedondoc.ls01@gmail.com>
2018-05-24 19:11:03 +0700

    ,asad

--- commands/watashi.js
+++ commands/watashi.js
@@ -13,21 +13,23 @@ exports.run = (Discord,rdc,client,message,cmd) =>{
 	ctx = canvas.getContext("2d")
 	try {
 		img.src = fs.readFileSync("src/info.png")
-		fs.readFile(message.author.avatarURL,(err,src)=>{
-			ava.src=src;
+		req.get(message.author.avatarURL.split("?")[0],{encoding:null},(err,res)=>{
+			ava.src = res.body
 		})
 	} catch (error) {
 		console.log(error)
 		return
 	}
-	ctx.drawImage(ava,25,25,150,150)
-	ctx.font = "30px Arial"
-	ctx.fillStyle = "rgba(255,255,255,1)"
-	ctx.fillText(name,300,25,200)
-	ctx.fillText(dcjoin,300,50)
-	ctx.fillText(svjoin,300,75)
-
-	canvas.createPNGStream().pipe(
-		fs.createWriteStream('src/info.png').on("close",()=>{message.channel.send(new Discord.Attachment("src/info.png"))})
-	)
+	ava.onload = ()=>{
+		ctx.drawImage(ava,25,25,150,150)
+		ctx.font = "30px Arial"
+		ctx.fillStyle = "rgba(255,255,255,1)"
+		ctx.fillText(name,300,25,200)
+		ctx.fillText(dcjoin,300,50)
+		ctx.fillText(svjoin,300,75)
+	
+		canvas.createPNGStream().pipe(
+			fs.createWriteStream('src/info.png').on("close",()=>{message.channel.send(new Discord.Attachment("src/info.png"))})
+		)
+	}
 }
