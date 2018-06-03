e7c9565f6b385d3406b2c7c0c1593ccfee196516
Yumi <caubedondoc.ls01@gmail.com>
2018-06-03 19:54:54 +0700

    asdfghjkl

--- commands/busy.js
+++ commands/busy.js
@@ -2,6 +2,34 @@ const Discord = require('discord.js')
 const rdc = require('redis').createClient(process.env.REDIS_URL);
 var list;
 
+exports.run = (client,message,args) => {
+    rdc.get('busy',(err,res)=>{
+        if(res===null){
+            list = []
+        }else{
+            list = JSON.parse(res.toString())
+        }
+    })
+    switch(args){
+        case 'on':
+            if(message.author.presence.status==='dnd'|| message.author.presence.status==='idle'){
+                turnon(message.author.id);
+                rdc.set("busy",JSON.stringify(list),()=>{
+                    message.channel.send(`${message.author} busy mode on`)
+                })
+            }else{
+                message.channel.send('you must be in `Do not disturb` or `Idle`')
+            }
+            return;
+        case 'off':
+            turnoff(message.author.id)
+            break;
+        default:
+            setContent(message.author.id,args);
+            break;
+    }
+}
+
 function turnon(id){
     let fi = list.findIndex(m=> m.id===id)
     if(fi!==-1){
@@ -37,33 +65,5 @@ function setContent(id,ct){
         rdc.set("busy",JSON.stringify(list),()=>{
             message.channel.send('your message is added, use `!busy on` to active')
         })
-    }
-}
-
-exports.run = (client,message,args) => {
-    rdc.get('busy',(err,res)=>{
-        if(res===null){
-            list = []
-        }else{
-            list = JSON.parse(res.toString())
-        }
-    })
-    switch(args){
-        case 'on':
-            if(message.author.presence.status==='dnd'|| message.author.presence.status==='idle'){
-                turnon(message.author.id);
-                rdc.set("busy",JSON.stringify(list),()=>{
-                    message.channel.send(`${message.author} busy mode on`)
-                })
-            }else{
-                message.channel.send('you must be in `Do not disturb` or `Idle`')
-            }
-            return;
-        case 'off':
-            turnoff(message.author.id)
-            break;
-        default:
-            setContent(message.author.id,args);
-            break;
     }
 }
