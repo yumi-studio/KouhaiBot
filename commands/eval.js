14da6a9335a063c86672045b89dd301912f1a53f
Yumi <caubedondoc.ls01@gmail.com>
2018-07-18 22:18:32 +0700

    .

---
+++ commands/eval.js
@@ -0,0 +1,14 @@
+exports.run = (client,message,cmd) =>{
+    if(message.author.id!== process.env.BOSS_ID) return
+    try {
+        let ev = eval(cmd)
+        if(typeof ev !== 'string'){
+            ev = require('util').inspect(ev)
+        }
+        let inp = '`'+cmd+'`'
+        let outp =  '`'+ev+'`'
+        message.channel.send(`input\n${inp}\noutput\n${outp}`)
+    } catch (error) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+}
