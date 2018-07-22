3fcd53094d9d97f5b7de49533f2445142201e5b6
Yumi <caubedondoc.ls01@gmail.com>
2018-07-22 21:42:23 +0700

    jkdjalkjaksd

--- commands/neko.js
+++ commands/neko.js
@@ -1,14 +1,25 @@
 const Discord = require('discord.js')
 const rq = require('request')
+var src = 'https://nekos.life/'
+var lewd = false
+var tag = ""
 exports.run=(client,message,cmd)=>{
-    if(message.channel.nsfw===false){
+	if(cmd.split(' ')[1]==='lewd'){
+		lewd=true
+		tag='lewd'
+	}
+    if(lewd && message.channel.nsfw===false){
         message.channel.send('( ͡° ͜ʖ ͡°) Please go to `NSFW` place')
         return
-    }
-    rq.get('https://nekos.life/lewd',(err,res,body)=>{
+	}
+    rq.get(src+tag,(err,res,body)=>{
 		let strindex = body.split('"')
 		let ans = strindex.find(m=>{
-			return m.indexOf('https://cdn.nekos.life/lewd')===0
+			if(lewd){
+				return m.indexOf('https://cdn.nekos.life/lewd')===0
+			}else{
+				return m.indexOf('https://cdn.nekos.life/neko')===0
+			}
 		})
 		message.channel.send(new Discord.RichEmbed().setImage(ans))
 	})
