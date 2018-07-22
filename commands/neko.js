d50565885f8b412c22934c7350c5d4dcc895898c
Yumi <caubedondoc.ls01@gmail.com>
2018-07-22 22:24:11 +0700

    zzzzzzzz

--- commands/neko.js
+++ commands/neko.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js')
 const rq = require('request')
-var src = 'https://nekos.life/'
-var lewd = false
-var tag = ""
+
 exports.run=(client,message,args)=>{
+	var src = 'https://nekos.life/'
+	var lewd = false
+	var tag = ""
 	if(args==='lewd'){
 		lewd=true
 		tag='lewd'
@@ -11,16 +12,18 @@ exports.run=(client,message,args)=>{
     if(lewd && message.channel.nsfw===false){
         message.channel.send('( ͡° ͜ʖ ͡°) Please go to `NSFW` place')
         return
-	}
-    rq.get(src+tag,(err,res,body)=>{
-		let strindex = body.split('"')
-		let ans = strindex.find(m=>{
-			if(lewd){
-				return m.indexOf('https://cdn.nekos.life/lewd')===0
-			}else{
-				return m.indexOf('https://cdn.nekos.life/neko')===0
-			}
+	}else{
+		rq.get(src+tag,(err,res,body)=>{
+			let strindex = body.split('"')
+			let ans = strindex.find(m=>{
+				if(lewd){
+					return m.indexOf('https://cdn.nekos.life/lewd')===0
+				}else{
+					return m.indexOf('https://cdn.nekos.life/neko')===0
+				}
+			})
+			message.channel.send(new Discord.RichEmbed().setImage(ans))
 		})
-		message.channel.send(new Discord.RichEmbed().setImage(ans))
-	})
+	}
+    
 }
