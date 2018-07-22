6e64ec8b15aad864fe068af71e278aec86bd16f0
Yumi <caubedondoc.ls01@gmail.com>
2018-07-22 11:25:20 +0700

    nsfw stuff ( ͡° ͜ʖ ͡°)

---
+++ commands/neko.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+const rq = require('request')
+exports.run=(client,message,cmd)=>{
+    if(message.channel.nsfw===false){
+        message.channel.send('( ͡° ͜ʖ ͡°) Please go to `NSFW` place')
+        return
+    }
+    rq.get('https://nekos.life/lewd',(err,res,body)=>{
+		let strindex = body.split('"')
+		let ans = strindex.find(m=>{
+			return m.indexOf('https://cdn.nekos.life/lewd')===0
+		})
+		message.channel.send(new Discord.RichEmbed().image(ans))
+	})
+}
