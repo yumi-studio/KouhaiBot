b3a70b1a3c04de6da75cdead4fa33b2d81f437ef
Yumi <caubedondoc.ls01@gmail.com>
2018-08-01 11:11:53 +0700

    zxzxxz

--- commands/rng.js
+++ commands/rng.js
@@ -8,7 +8,8 @@ exports.run=(client,message,args)=>{
         case 's':
             try {
                 arr = x[1].split('-')
-                message.channel.send(`Result: ${process(arr[0],arr[1])}`)
+                res = process(arr[0],arr[1])
+                message.channel.send(`Result: ${res}`)
             } catch (error) {
                 console.log()
             }
@@ -17,9 +18,10 @@ exports.run=(client,message,args)=>{
             try {
                 arr = x[2].split('-')
                 while(x[1]!=0){
-                    res+=process(arr[1],arr[2])
+                    res+=process(arr[1],arr[2])+" "
                     x[1]--
                 }
+                message.channel.send(res)
             } catch (error) {
                 console.log("rng error:\n"+error)
             }
@@ -28,6 +30,6 @@ exports.run=(client,message,args)=>{
     }
 
     function process(x,y){
-        return Math.floor(Math.random()*(y+1-x))+x;
+        return Math.floor(Math.random()*(y+1-x))+x
     }
 }
