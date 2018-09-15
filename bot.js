b2f42197215d7ccef91912894df5838111828e76
Yumi <caubedondoc.ls01@gmail.com>
2018-09-15 08:45:35 +0700

    web???

--- bot.js
+++ bot.js
@@ -6,6 +6,17 @@ const fs = require('fs');
 const yt = require('ytdl-core');
 const rdc = require('redis').createClient(process.env.REDIS_URL);
 
+var express = require('express')
+var app=express()
+var port = process.env.PORT || 8080
+app.use(express.static(__dirname+"/html"))
+app.get("/",function(req,res){
+	res.render("index")
+})
+app.listen(port,function(){
+	console.log("website online!")
+})
+
 const prefix = '!';
 
 client.on('ready', () => {
