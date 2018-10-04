3eb86dc6fea3fa30d341c35f540415e20195389f
Yumi <caubedondoc.ls01@gmail.com>
2018-10-04 19:33:57 +0700

    123

--- commands/wc.js
+++ commands/wc.js
@@ -9,7 +9,11 @@ exports.run = (client,message,cmd) =>{
     let channel = message.channel;
     let sender = message.author;
     let list=[];
-    if(sender.id!=process.env.BOSS_ID && message.member.permissions.toArray().indexOf("ADMINISTRATOR")==-1) return;
+    if(sender.id!=process.env.BOSS_ID){
+        if(!message.member.permissions.hasPermission("ADMINISTRATOR")){
+            return
+        }
+    }
     rdc.get("welcome"+guild.id,(err,reply)=>{
         if(reply!==null){
             list = JSON.parse(reply.toString());
